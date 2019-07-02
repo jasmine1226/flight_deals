531581b2877c9913d7be58803dda3d3be8ed5ff0
Jasmine Chen <jasmine1226@gmail.com>
2019-07-02 06:57:30 +0000

    Added DealScraper class and required environment inbin/flight_deals

---
+++ lib/flight_deals/deal_scraper.rb
@@ -0,0 +1,15 @@
+class FlightDeals::DealScraper
+
+  def self.scrape_deals(url)
+    deals = []
+
+    deals
+  end
+
+  def self.scrape_deal_info(url)
+    deal_info = {}
+
+    deal_info
+  end
+
+end
