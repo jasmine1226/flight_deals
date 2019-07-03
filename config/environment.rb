0ca9a66a28a80ac445a79fc9f6e53ae6b4ba7511
Jasmine Chen <jasmine1226@gmail.com>
2019-07-03 06:07:19 +0000

    Added DealScraper.scrape_deals and required 'open-uri' in environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative '../lib/flight_deals/version'
-require_relative '../lib/flight_deals/cli'
 require_relative '../lib/flight_deals/deal'
+require_relative '../lib/flight_deals/deal_scraper'
+require_relative '../lib/flight_deals/cli'
--- lib/flight_deals/deal_scraper.rb
+++ lib/flight_deals/deal_scraper.rb
@@ -2,11 +2,19 @@ class FlightDeals::DealScraper
 
   def self.scrape_deals(url)
     deals = []
-
+    doc = Nokogiri::HTML(open(url))
+    scraped_deals = doc.css("article.category-depart-usa>div.article-content-wrapper")
+    scraped_deals.each_with_index do |scraped_deal, i|
+      title = scraped_deal.css("h2 a").text
+      date = scraped_deal.css("div.entry-bottom-details span a time").text
+      description = scraped_deal.css("p").text
+      url = scraped_deal.css("h2 a").attribute("href").value
+      deals << FlightDeals::Deal.create(title, date, description, url)
+    end
     deals
   end
 
-  def self.scrape_deal_info(url)
+  def self.scrape_deal_page(url)
     deal_info = {}
 
     deal_info
