2ef36779a6ea24a70bacd446e0648e04a56e62a8
Jasmine Chen <jasmine1226@gmail.com>
2019-07-04 03:03:12 +0000

    Added deal elements from deal page, added Deal.all, Deal.save, & Deal.find_by_url(url)

--- lib/flight_deals/deal.rb
+++ lib/flight_deals/deal.rb
@@ -1,12 +1,27 @@
 class FlightDeals::Deal
-  attr_accessor :title, :date, :description, :url
+  attr_accessor :title, :post_date, :description, :url, :depart, :arrive, :dates, :stops, :airlines
 
-  def self.create(title, date, description, url)
+  @@all = []
+
+  def self.create(title, post_date, description, url)
     deal = self.new
     deal.title = title
-    deal.date = date
+    deal.post_date = post_date
     deal.description = description
     deal.url = url
+    deal.save
     deal
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    FlightDeals::Deal.all << self
+  end
+
+  def self.find_by_url(url)
+    deal = self.all.detect{|deal| deal.url == url}
   end
 end
