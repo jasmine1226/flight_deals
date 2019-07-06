c9d75c679f371d03c0822443de5d043e94590b6a
Jasmine Chen <jasmine1226@gmail.com>
2019-07-06 00:43:22 +0000

    Allowed DealScraper to load multiple pages and for CLI to navigate through pages.

--- lib/flight_deals/cli.rb
+++ lib/flight_deals/cli.rb
@@ -1,19 +1,44 @@
 class FlightDeals::CLI
-  attr_reader :deal
+  attr_reader :deal, :current_page, :loaded_page
 
   def call
-    list_deals
+    @current_page = 1
+    @loaded_page = 10
+    @deals = FlightDeals::DealScraper.scrape_deals(@loaded_page)
+    list_deals(@current_page)
     menu
   end
 
-  def list_deals
+  def list_deals(page=1)
     puts "Here are the latest flight deals departing from USA:"
-    @deals = FlightDeals::DealScraper.scrape_deals('https://www.secretflying.com/usa-deals/')
-    @deals.each_with_index do |deal, i|
-      puts "#{i+1}. #{deal.title} - #{deal.post_date}"
+    for i in (page-1)*9..page*9-1 do
+        puts "#{i+1}. #{@deals[i].title} - #{@deals[i].post_date}"
     end
-    puts "Enter a number between 1 to #{@deals.length} to view deal details, 'deals' to view the list of deals, or 'exit'"
-    @deals
+    puts "Enter a number between #{(page-1)*9+1} to #{page*9} to view deal details;"
+    puts "Enter 'deals' to see the deals again;"
+    puts "Enter 'next' to the next page;"
+    puts "Enter 'back' to the previous page;"
+    puts "Or enter 'exit'"
+  end
+
+  def next_page
+      @current_page += 1
+      if @current_page > 10
+        puts "No more deals to display. Come back tomorrow for new deals!"
+        @current_page = 10
+      else
+        list_deals(@current_page)
+      end
+  end
+
+  def prev_page
+    @current_page -= 1
+    if @current_page < 1
+      puts "Sorry, you're at the first page and cannot go back further."
+      @current_page = 1
+    else
+      list_deals(@current_page)
+    end
   end
 
   def goodbye
@@ -31,13 +56,16 @@ class FlightDeals::CLI
         deal = FlightDeals::DealScraper.scrape_deal_page(url)
         deal.display
       elsif input == "deals"
-        list_deals
+        list_deals(@current_page)
+      elsif input == "next"
+        next_page
+      elsif input == "back"
+        prev_page
       elsif input == "exit"
         goodbye
       else
         puts "Sorry, I'm not sure what you're looking for..."
-        list_deals
-      end
+        end
     end
   end
 
--- lib/flight_deals/deal_scraper.rb
+++ lib/flight_deals/deal_scraper.rb
@@ -1,22 +1,29 @@
 class FlightDeals::DealScraper
 
-  def self.scrape_deals(url)
+  def self.load_page(url)
+    Nokogiri::HTML(open(url))
+  end
+
+  def self.scrape_deals(page=10)
     deals = []
-    doc = Nokogiri::HTML(open(url))
-    scraped_deals = doc.css("article.category-depart-usa>div.article-content-wrapper")
-    scraped_deals.each_with_index do |scraped_deal, i|
-      title = scraped_deal.css("h2 a").text
-      post_date = scraped_deal.css("div.entry-bottom-details span a time").text
-      description = scraped_deal.css("p").text
-      url = scraped_deal.css("h2 a").attribute("href").value
-      deals << FlightDeals::Deal.create(title, post_date, description, url)
+    for i in 1..page do
+      doc = FlightDeals::DealScraper.load_page("https://www.secretflying.com/usa-deals/page/#{i+1}/")
+      scraped_deals = doc.css("article.category-depart-usa>div.article-content-wrapper")
+      scraped_deals.each_with_index do |scraped_deal, i|
+        title = scraped_deal.css("h2 a").text
+        post_date = scraped_deal.css("div.entry-bottom-details span a time").text
+        description = scraped_deal.css("p").text
+        url = scraped_deal.css("h2 a").attribute("href").value
+        deals << FlightDeals::Deal.create(title, post_date, description, url)
+      end
     end
     deals
   end
 
   def self.scrape_deal_page(url)
-    doc = Nokogiri::HTML(open(url))
+    doc = FlightDeals::DealScraper.load_page(url)
     deal = FlightDeals::Deal.find_by_url(url)
+
     deal.depart = doc.at('strong:contains("DEPART:")').next_element.next_element.text
     deal.arrive = doc.at('strong:contains("ARRIVE:")').next_element.next_element.text
     deal.dates = doc.at('strong:contains("DATES:")').next_element.next_element.text
